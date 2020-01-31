ab3f83c293c75f9cb452ebde605922bb3e1da971
root <root@ip-172-31-84-240.ec2.internal>
2020-01-31 12:52:18 +0000

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          get stted, edit <code>src/App.js</code> and save to reload.
+          get stated, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
